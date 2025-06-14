d87b6ba28786b466a4798fd688aad69481d63602
Julius Simonelli <julius.simonelli@gmail.com>
2025-06-14 11:37:57 -0700

    Add improved Skulk/Rebuild strategy

---
+++ dominion/strategy/strategies/skulk_rebuild_improved.py
@@ -0,0 +1,66 @@
+from dominion.strategy.enhanced_strategy import EnhancedStrategy, PriorityRule
+
+
+class SkulkRebuildImprovedStrategy(EnhancedStrategy):
+    """Improved strategy for the Forager/Skulk/Rebuild board."""
+
+    def __init__(self):
+        super().__init__()
+        self.name = "SkulkRebuildImproved"
+        self.description = (
+            "Enhanced Forager/Skulk/Rebuild plan with extra trashing and better late game focus"
+        )
+        self.version = "1.1"
+
+        # Gain priorities
+        self.gain_priority = [
+            # Province when affordable
+            PriorityRule("Province", PriorityRule.can_afford(8)),
+            # Rebuild up to three copies early
+            PriorityRule(
+                "Rebuild",
+                PriorityRule.and_(PriorityRule.provinces_left(">", 2), "my.count(Rebuild) < 3"),
+            ),
+            # Duchies for points
+            PriorityRule("Duchy"),
+            # Pick up Skulks for Gold gain
+            PriorityRule("Skulk", PriorityRule.can_afford(4)),
+            # Two Foragers to accelerate trashing
+            PriorityRule("Forager", "my.count(Forager) < 2"),
+            # Gain Gold if affordable
+            PriorityRule("Gold", PriorityRule.can_afford(6)),
+            # Silver only early
+            PriorityRule(
+                "Silver",
+                PriorityRule.and_(PriorityRule.provinces_left(">", 3), PriorityRule.can_afford(3)),
+            ),
+            # Estates once Provinces are nearly gone
+            PriorityRule("Estate", PriorityRule.provinces_left("<=", 2)),
+            PriorityRule("Copper"),
+        ]
+
+        # Action priorities
+        self.action_priority = [
+            # Use Forager early to thin the deck
+            PriorityRule("Forager"),
+            PriorityRule("Rebuild"),
+            PriorityRule("Skulk"),
+        ]
+
+        # Trash priorities
+        self.trash_priority = [
+            PriorityRule("Curse"),
+            PriorityRule("Estate"),
+            PriorityRule("Copper"),
+        ]
+
+        # Treasure play order
+        self.treasure_priority = [
+            PriorityRule("Gold"),
+            PriorityRule("Silver"),
+            PriorityRule("Copper"),
+        ]
+
+
+def create_skulk_rebuild_improved() -> EnhancedStrategy:
+    return SkulkRebuildImprovedStrategy()
