35dbf4d2879a526bdb33fd406d31592d8c0144a0
Julius Simonelli <julius.simonelli@gmail.com>
2025-06-15 09:59:27 -0700

    Add Acting Troupe card with Villagers

--- dominion/cards/renaissance/acting_troupe.py
+++ dominion/cards/renaissance/acting_troupe.py
@@ -9,3 +9,19 @@ class ActingTroupe(Card):
             stats=CardStats(),
             types=[CardType.ACTION],
         )
+
+    def play_effect(self, game_state):
+        player = game_state.current_player
+        player.villagers += 4
+        game_state.log_callback(
+            (
+                "action",
+                player.ai.name,
+                "gains 4 Villagers",
+                {"villagers_total": player.villagers},
+            )
+        )
+
+        if self in player.in_play:
+            player.in_play.remove(self)
+        game_state.trash_card(player, self)
