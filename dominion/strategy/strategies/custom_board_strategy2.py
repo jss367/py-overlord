1b122f0027b9c59e08531b0ea7e9431b670c7486
Julius Simonelli <julius.simonelli@gmail.com>
2025-06-14 23:03:58 -0700

    Create custom_board_strategy2.py

---
+++ dominion/strategy/strategies/custom_board_strategy2.py
@@ -0,0 +1,62 @@
+from dominion.strategy.enhanced_strategy import EnhancedStrategy, PriorityRule
+
+
+class CustomBoardStrategy2(EnhancedStrategy):
+    """Strategy generated from genetic algorithm for a custom Patrician board."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "CustomBoard"
+        self.description = "Genetic algorithm result for Patrician/Emporium board"
+        self.version = "1.0"
+
+        # Gain priorities
+        self.gain_priority = [
+            PriorityRule("Province", PriorityRule.can_afford(8)),
+            PriorityRule("Emporium"),
+            PriorityRule("Patrician"),
+            PriorityRule("Forager"),
+            PriorityRule("Snowy Village", PriorityRule.turn_number("<=", 10)),
+            PriorityRule("Rebuild"),
+            PriorityRule("Modify"),
+            PriorityRule("Collection"),
+            PriorityRule("Skulk"),
+            PriorityRule("Miser"),
+            PriorityRule("Looting", PriorityRule.can_afford(5)),
+            PriorityRule("Duchy", PriorityRule.provinces_left("<=", 4)),
+            PriorityRule("Gold", PriorityRule.can_afford(6)),
+            PriorityRule("Silver", PriorityRule.can_afford(3)),
+            PriorityRule("Estate", PriorityRule.provinces_left("<=", 2)),
+        ]
+
+        # Action priorities
+        self.action_priority = [
+            PriorityRule("Forager"),
+            PriorityRule("Modify"),
+            PriorityRule("Rebuild"),
+            PriorityRule("Patrician"),
+            PriorityRule("Emporium"),
+            PriorityRule("Snowy Village"),
+            PriorityRule("Skulk"),
+            PriorityRule("Miser"),
+            PriorityRule("Rats"),
+        ]
+
+        # Trash priorities
+        self.trash_priority = [
+            PriorityRule("Curse"),
+            PriorityRule("Rats"),
+            PriorityRule("Estate"),
+            PriorityRule("Copper"),
+        ]
+
+        # Treasure play order
+        self.treasure_priority = [
+            PriorityRule("Gold"),
+            PriorityRule("Silver"),
+            PriorityRule("Copper"),
+        ]
+
+
+def create_custom_board_strategy2() -> EnhancedStrategy:
+    return CustomBoardStrategy2()
