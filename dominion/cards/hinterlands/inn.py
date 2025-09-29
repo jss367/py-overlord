bfbc56dc7470253f0f3d9a87509e5c1d2dabe5fd
Julius Simonelli <julius.simonelli@gmail.com>
2025-09-29 14:30:58 -0700

    Implement Hinterlands card behaviors (#81)

--- dominion/cards/hinterlands/inn.py
+++ dominion/cards/hinterlands/inn.py
@@ -1,3 +1,5 @@
+import random
+
 from ..base_card import Card, CardCost, CardStats, CardType
 
 
@@ -9,3 +11,22 @@ class Inn(Card):
             stats=CardStats(actions=2, cards=2),
             types=[CardType.ACTION],
         )
+
+    def on_gain(self, game_state, player):
+        """Shuffle any Actions from the discard pile back into the deck."""
+
+        super().on_gain(game_state, player)
+
+        # Gather the action cards currently in the discard pile.
+        action_cards = [card for card in player.discard if card.is_action]
+
+        if not action_cards:
+            return
+
+        # Remove the chosen cards from the discard pile.
+        for card in action_cards:
+            player.discard.remove(card)
+
+        # Shuffle them into the current deck.
+        player.deck.extend(action_cards)
+        random.shuffle(player.deck)
